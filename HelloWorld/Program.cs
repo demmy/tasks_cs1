498c2c7b657679c8e25da4dcc53b9bc966c00723
SalamanderSunburn <taisiya.loza@gmail.com>
2015-08-04 11:07:23 +0300

    TaskCommit1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Strange merge...");
+
+            Console.ReadLine();
         }
     }
 }
