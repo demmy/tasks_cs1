67c4eb47481207b69e10dba1ae3cdf157016edb4
marakonec <marakonec@gmail.com>
2015-08-04 11:08:00 +0300

    firstChange

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -10,6 +10,9 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+            string message = "Change hello wold file";
+            Console.WriteLine(message);
+            Console.ReadKey();
         }
     }
 }
