a67d25fd900270b0199e400621e57c2b48c3b836
Serge <matvienko.serega2012@gmail.com>
2015-08-07 14:41:10 +0300

    Unneeded commands removed

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,37 +1,4 @@
-﻿<<<<<<< HEAD
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HelloWorld
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            string message = "Change hello world file";
-            Console.WriteLine(message);
-            Console.WriteLine("+========================================+");
-            Console.WriteLine("+         HELLO     WORLD                +");
-            Console.WriteLine("+=======================================+");
-            Console.ReadKey();
-            Console.WriteLine("Strange merge...");
-            Console.WriteLine("Well, hello creepy world...");
-            
-            
-            Console.ReadLine();
-            StringBuilder str= new StringBuilder("Hello!");
-            str.Clear();
-            str.AppendLine("This world is beautiful! And this string is changeable!");
-            Console.WriteLine("Good Day! {0}", str.ToString());
-            Console.ReadKey();
-        }
-    }
-}
-=======
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -58,4 +25,3 @@ namespace HelloWorld
         }
     }
 }
->>>>>>> helloWorldvserge
