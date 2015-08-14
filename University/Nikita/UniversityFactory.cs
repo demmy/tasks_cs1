2bb768a436797185a66350424821124b0e266d7f
Olha Vakhromova <olga.jane.vakhromova@gmail.com>
2015-08-14 12:51:24 +0300

    university task

---
+++ University/Nikita/UniversityFactory.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Nikita
+{
+    class UniversityFactory : IUniversityFactory
+    {
+        public IUniversity CreateUniversity(string title)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ProgrammerName
+        {
+            get { throw new NotImplementedException(); }
+        }
+    }
+}
