60d3ea88ef2c43b7ac308cc103503252318434ee
imnikoo <efruev@gmail.com>
2015-09-14 15:24:52 +0300

    program

---
+++ Collections/Nikita/CollectionsExercises.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections.Nikita
+{
+    class CollectionsExercises : ICollectionsExercises
+    {
+        public void WorkPriorityQueue()
+        {
+            MyLogger logger = new MyLogger();
+            MySingleCollection<int> coll = new MySingleCollection<int>(logger);
+            coll.Add(5);
+            coll.Add(4);
+            coll.Add(2);
+            coll.Add(40);
+            coll.Add(2);
+            coll.Add(1);
+
+            coll.RemoveAt(2);
+            coll.Clear();
+            logger.ShowAllItems();
+            
+        }
+    }
+}
