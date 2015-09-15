0f0a3feacb5adc94ce22c301530f4f300e8c47a4
imnikoo <efruev@gmail.com>
2015-09-15 17:38:13 +0300

    small change
    added enum Lights

---
+++ AutomobilesNikita/Automobiles/Automobiles/Facade.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Automobiles
+{
+    public partial class Facade : Form
+    {
+        public Facade()
+        {
+            InitializeComponent();
+        }
+    }
+}
