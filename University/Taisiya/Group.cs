75b5ebf790d287bf3a3bc15e56cce9ac8284e0ef
SalamanderSunburn <taisiya.loza@gmail.com>
2015-09-27 23:56:51 +0300

    UNI done commit

---
+++ University/Taisiya/Group.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University.Taisiya
+{
+    class Group: IReadOnlyGroup
+    {
+        private string id;
+        private FacultyType faculty;
+        private IEnumerable<IReadOnlyPerson> students;
+        private int year;
+        SpecialityTitle speciality;
+        private static Dictionary<string, int> existingGroupsCount;
+        private int count;
+
+        public Group(FacultyType faculty, SpecialityTitle speciality, int year)
+        {
+            this.faculty = faculty;
+            this.speciality = speciality;
+            this.year = year;
+                      
+            foreach (var ec in existingGroupsCount.Keys)
+            {
+                if (Title != ec)
+                    count = 1;
+                else
+                    count++;
+            }
+            existingGroupsCount.Add(Title, count);
+        }
+
+        public string Title
+        {
+            get 
+            {
+                string name = Speciality.ToString().Select(x => Char.IsUpper(x)).ToString()
+                    + Faculty.ToString().Select(x => Char.IsUpper(x)).ToString() 
+                    + "-" + year.ToString()[2] + year.ToString()[3];               
+
+                return name; 
+            }
+        }
+
+        public string ID
+        {
+            get 
+            {                           
+                return ID + " - " + count; 
+            }
+        }
+
+        public SpecialityTitle Speciality
+        {
+            get { return speciality; }
+        }
+
+        public FacultyType Faculty
+        {
+            get { return faculty; }
+        }
+
+        public IEnumerable<IReadOnlyPerson> Students
+        {
+            get { return students; }
+        }
+
+        public void Add(Student s)
+        {
+            students.ToList().Add((IReadOnlyPerson)s);
+        }
+    }
+}
